cac0c684c31c2a411dce50a1a8a7cd05dcc285c3
Benjamin Buske <info@buske-it.com>
2025-03-05 12:31:13 -0400

    initial commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+// DB Params
+
+const DB_HOST = "localhost";
+const DB_USER = "bbuske";
+const DB_PASS = "GeF37CJh#";
+const DB_NAME = "joblister";
+
+// Site Title
+const SITE_TITLE = 'JobLister';
---
+++ templates/inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JobLister</title>
+    <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+<div class="container">
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation"><a href="../../index.php">Home</a></li>
+                <li role="presentation"><a href="create.php">Create Listing</a></li>
+            </ul>
+        </nav>
+        <h3 class="text-muted"><?php echo SITE_TITLE; ?></h3>
+    </div>
